d32857a668db862847a142ee95460a32226bc17a
EmiriMaung <poelay1995@gmail.com>
2026-06-11 14:39:34 +0630

    feat: add new MVC project

---
+++ BookReadingTracker.MVC/Controllers/ReadingListController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookReadingTracker.MVC.Controllers
+{
+    public class ReadingListController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BookReadingTracker.MVC/Controllers/UserController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookReadingTracker.MVC.Controllers
+{
+    public class UserController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
